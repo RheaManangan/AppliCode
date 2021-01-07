92cd0ce0522635bc51d06718c6615e567a6df796
RheaManangan <rheamanangan.bsit.ucu@gmail.com>
2021-01-07 15:34:38 +0800

    Project

---
+++ app/src/main/java/com/example/applicode/CandSClass.java
@@ -0,0 +1,24 @@
+package com.example.applicode;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class CandSClass extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.c_and_s);
+
+
+
+
+
+    }
+}
